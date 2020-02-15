5a547f0da178e67ab9fbeee439b7efe6698ffbfc
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-02-16 01:48:14 +0700

    Add files via upload

---
+++ Unit 4 Volumetrics/functions/simpson.py
@@ -0,0 +1,10 @@
+def simpson(y_vals, h):
+    i = 1
+    total = y_vals[0] + y_vals[-1]
+    for y in y_vals[1:-1]:
+        if i % 2 == 0:
+            total += 2 * y
+        else:
+            total += 4 * y
+        i += 1
+    return total * (h / 3.0)
