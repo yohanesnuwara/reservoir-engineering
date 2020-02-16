c019879d844ff657d66bf4de0d6e1eec1cff93c7
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-02-16 12:01:55 +0700

    Update simpson.py

--- Unit 4 Volumetrics/functions/simpson.py
+++ Unit 4 Volumetrics/functions/simpson.py
@@ -1,4 +1,6 @@
 def simpson(y_vals, h):
+    # y_vals: data of isocontours, e.g. areas of isocontours, see Table 4.3 for example
+    # h: difference between isocontours, regular space, see Table 4.3 for example
     i = 1
     total = y_vals[0] + y_vals[-1]
     for y in y_vals[1:-1]:
