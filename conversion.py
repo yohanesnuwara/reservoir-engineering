7e0dba58fba9d54ec2af4f894f563f29e69c0158
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-02-17 23:47:15 +0700

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,29 @@
+""""
+1 API 		= 141.4 / (131.5 + API) g/cm3
+1 atm 		= 1.013250E+05 Pa
+1 bbl 		= 1.589873E-01 m
+1 cp 		= 1E-03 Pa.s
+1 darcy 	= 9.869233 micro.m2
+1 dyne 		= 1E-02 mN
+1 dyne/cm2 	= 1E-01 Pa
+1 ft 		= 3.048E-01 m 
+1 ft2 		= 9.290304E-02 m2
+1 ft3 (scf) 	= 2.831685E-02 m2
+1 F 		= (F - 32) / 1.8 C
+1 F 		= (F + 459.67) / 1.8 K
+1 milidarcy 	= 9.869233E-04 micro.m2
+1 in 		= 2.54 cm
+1 in2 		= 6.4516 cm2
+1 in3 		= 1.638706 cm3
+1 lbf 		= 4.448706 N
+1 lbm 		= 4.535924E-01 kg
+1 psi 		= 6.894757 kPa
+1 psi^-1 	= 1.450377E-01 kPa^-1
+1 R 		= 5/9 K
+
+1 acre 		= 4.046E-01 ha
+1 acre-ft 	= 1.233489E+03 m
+1 mile 		= 1.609344 km
+1 sq mile 	= 2.589988 km2
+
+""""
