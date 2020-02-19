42957ea39ba73994e2c094a9855f373ce0fa3e3b
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-02-19 20:05:57 +0700

    Update gridder.py

--- Unit 6 Single-Phase-Fluid Flow in Porous Media/functions/gridder.py
+++ Unit 6 Single-Phase-Fluid Flow in Porous Media/functions/gridder.py
@@ -1,134 +1,135 @@
 "Code from Fatiando"
 
+"""
+2D interpolation, griding, and profile extraction.
+"""
 from __future__ import division, absolute_import, print_function
-import numpy.testing as npt
 import numpy as np
+import scipy.interpolate
 
-from ... import gridder
-from ..interpolation import fill_nans
-
-
-def test_fill_nans():
-    "fill_nans recovers values that were removed from a simple grid"
-    x, y, z = gridder.regular((0, 20, -10, 2), (10, 10), z=500)
-    missing = np.zeros(x.shape, dtype=np.bool)
-    missing[[0, 15, 67, 32, 12]] = True
-    z_missing = np.copy(z)
-    z_missing[missing] = np.nan
-    assert np.any(z_missing != z)
-    fill_nans(x, y, z, x, y, z_missing)
-    npt.assert_allclose(z_missing, z)
-    # Test using a masked array as well
-    mask = np.zeros_like(z)
-    mask[[35, 43, 1, 46, 83, 99, 14, 78]] = 1
-    z_missing = np.ma.masked_array(z, mask=mask)
-    assert np.any(z_missing.mask)
-    fill_nans(x, y, z, x, y, z_missing)
-    assert np.all(~z_missing.mask)
-    npt.assert_allclose(z_missing, z)
+from .point_generation import regular
 
 
-def test_interp_at():
-    "Interpolate a few points of smooth data gives correct results"
-    # Generate some smooth data
-    x, y = gridder.regular([0, 10, -10, -5], (50, 50))
-    data = x**2 + y**2
-    # Remove some points and interpolate them
-    kept = np.ones(data.shape, dtype=np.bool)
-    kept[[10, 51, 456, 2000, 1501]] = False
-    removed = ~kept
-    for algorithm in ['linear', 'cubic']:
-        data_interp = gridder.interp_at(x[kept], y[kept], data[kept],
-                                        x[removed], y[removed],
-                                        algorithm=algorithm,
-                                        extrapolate=False)
-        npt.assert_allclose(data[removed], data_interp, rtol=0.01)
+def fill_nans(x, y, v, xp, yp, vp):
+    """"
+    Fill in the NaNs or masked values on interpolated points using nearest
+    neighbors.
+    .. warning::
+        Operation is performed in place. Replaces the NaN or masked values of
+        the original array!
+    Parameters:
+    * x, y : 1D arrays
+        Arrays with the x and y coordinates of the original data points (not
+        interpolated).
+    * v : 1D array
+        Array with the scalar value assigned to the data points (not
+        interpolated).
+    * xp, yp : 1D arrays
+        Points where the data values were interpolated.
+    * vp : 1D array
+        Interpolated data values (the one that has NaNs or masked values to
+        replace).
+    """
+    if np.ma.is_masked(vp):
+        nans = vp.mask
+    else:
+        nans = np.isnan(vp)
+    vp[nans] = scipy.interpolate.griddata((x, y), v, (xp[nans], yp[nans]),
+                                          method='nearest').ravel()
 
 
-def test_interp_at_extrapolation():
-    "Interpolate and extrapolate edge points that are not included"
-    # Generate some smooth data
-    x, y = gridder.regular([0, 10, -10, -5], (50, 50))
-    data = np.ones_like(x)
-    # Remove some points and interpolate them
-    kept = np.ones(data.shape, dtype=np.bool)
-    # All these points should interpolate to nan using cubic or linear because
-    # they are on the edges of the grid and outside the convex hull of the kept
-    # data. Using extrapolate=True will calculate these points use nearest
-    # neighbors.
-    kept[[0, 49, 2449, 2499]] = False
-    removed = ~kept
-    args = (x[kept], y[kept], data[kept], x[removed], y[removed])
-    for algorithm in ['linear', 'cubic']:
-        data_interp = gridder.interp_at(*args, algorithm=algorithm,
-                                        extrapolate=False)
-        # Make sure they are nans when using extrapolate=False
-        assert np.all(np.isnan(data_interp))
-        # Now test with extrapolation
-        data_interp = gridder.interp_at(*args, algorithm=algorithm,
-                                        extrapolate=True)
-        npt.assert_allclose(data[removed], data_interp, rtol=0.01)
+def interp_at(x, y, v, xp, yp, algorithm='cubic', extrapolate=False):
+    """
+    Interpolate spacial data onto specified points.
+    Wraps ``scipy.interpolate.griddata``.
+    Parameters:
+    * x, y : 1D arrays
+        Arrays with the x and y coordinates of the data points.
+    * v : 1D array
+        Array with the scalar value assigned to the data points.
+    * xp, yp : 1D arrays
+        Points where the data values will be interpolated
+    * algorithm : string
+        Interpolation algorithm. Either ``'cubic'``, ``'nearest'``,
+        ``'linear'`` (see scipy.interpolate.griddata)
+    * extrapolate : True or False
+        If True, will extrapolate values outside of the convex hull of the data
+        points.
+    Returns:
+    * v : 1D array
+        1D array with the interpolated v values.
+    """
+    vp = scipy.interpolate.griddata((x, y), v, (xp, yp),
+                                    method=algorithm).ravel()
+    if extrapolate and algorithm != 'nearest' and np.any(np.isnan(vp)):
+        fill_nans(x, y, v, xp, yp, vp)
+    return vp
 
 
-def test_interp():
-    "Test interpolating on a regular grid using smooth data"
-    # Generate some smooth data on a scatter of points
-    area = [0, 10, -10, -5]
-    # Use a lot of points to get a good interpolation
-    x, y = gridder.scatter(area, n=50000, seed=0)
+def interp(x, y, v, shape, area=None, algorithm='cubic', extrapolate=False):
+    """
+    Interpolate spacial data onto a regular grid.
+    Utility function that generates a regular grid with
+    :func:`~fatiando.gridder.regular` and calls
+    :func:`~fatiando.gridder.interp_at` on the generated points.
+    Parameters:
+    * x, y : 1D arrays
+        Arrays with the x and y coordinates of the data points.
+    * v : 1D array
+        Array with the scalar value assigned to the data points.
+    * shape : tuple = (nx, ny)
+        Shape of the interpolated regular grid, ie (nx, ny).
+    * area : tuple = (x1, x2, y1, y2)
+        The are where the data will be interpolated. If None, then will get the
+        area from *x* and *y*.
+    * algorithm : string
+        Interpolation algorithm. Either ``'cubic'``, ``'nearest'``,
+        ``'linear'`` (see scipy.interpolate.griddata).
+    * extrapolate : True or False
+        If True, will extrapolate values outside of the convex hull of the data
+        points.
+    Returns:
+    * ``[x, y, v]``
+        Three 1D arrays with the interpolated x, y, and v
+    """
+    if area is None:
+        area = (x.min(), x.max(), y.min(), y.max())
+    x1, x2, y1, y2 = area
+    xp, yp = regular(area, shape)
+    vp = interp_at(x, y, v, xp, yp, algorithm=algorithm,
+                   extrapolate=extrapolate)
+    return xp, yp, vp
 
-    def makedata(x, y):
-        return x**3 + y**2
 
-    data = makedata(x, y)
-    shape = (50, 50)
-    for algorithm in ['linear', 'cubic']:
-        # Test passing in a specified area equal the true area
-        xp, yp, datap = gridder.interp(x, y, data, shape, area=area,
-                                       algorithm=algorithm, extrapolate=True)
-        xpt, ypt = gridder.regular(area, shape)
-        npt.assert_allclose(xp, xpt)
-        npt.assert_allclose(yp, ypt)
-        npt.assert_allclose(datap, makedata(xp, yp), rtol=0.05)
-        # Test letting the function determine the area from the inputs
-        xp, yp, datap = gridder.interp(x, y, data, shape, area=None,
-                                       algorithm=algorithm, extrapolate=True)
-        xpt, ypt = gridder.regular((xp.min(), xp.max(), yp.min(), yp.max()),
-                                   shape)
-        npt.assert_allclose(xp, xpt)
-        npt.assert_allclose(yp, ypt)
-        npt.assert_allclose(datap, makedata(xp, yp), rtol=0.05)
-
-
-def test_profile():
-    "Extracting a profile from smooth data works"
-    # Generate some smooth data
-    area = [0, 50, -50, 0]
-    shape = (51, 51)
-    x, y = gridder.regular(area, shape)
-
-    def makedata(x, y):
-        return x**3 + y**2
-
-    data = makedata(x, y)
-    for algorithm in ['linear', 'cubic']:
-        # Test with two points along the x axis
-        p1 = [20, 0]
-        p2 = [20, -50]
-        n = shape[0]
-        xp, yp, dist, datap = gridder.profile(x, y, data, p1, p2, n,
-                                              algorithm=algorithm)
-        npt.assert_allclose(dist, np.linspace(0, area[1] - area[0], n))
-        npt.assert_almost_equal(xp, np.zeros_like(xp) + 20)
-        npt.assert_allclose(yp, np.linspace(area[2], area[3], shape[1])[::-1])
-        npt.assert_allclose(datap, makedata(xp, yp))
-        # Test with two points along the y axis
-        p1 = [0, -25]
-        p2 = [50, -25]
-        n = shape[1]
-        xp, yp, dist, datap = gridder.profile(x, y, data, p1, p2, n,
-                                              algorithm=algorithm)
-        npt.assert_allclose(dist, np.linspace(0, area[3] - area[2], n))
-        npt.assert_almost_equal(yp, np.zeros_like(yp) - 25)
-        npt.assert_allclose(xp, np.linspace(area[0], area[1], shape[0]))
-        npt.assert_allclose(datap, makedata(xp, yp))
+def profile(x, y, v, point1, point2, size, algorithm='cubic'):
+    """
+    Extract a profile between 2 points from spacial data.
+    Uses interpolation to calculate the data values at the profile points.
+    Parameters:
+    * x, y : 1D arrays
+        Arrays with the x and y coordinates of the data points.
+    * v : 1D array
+        Array with the scalar value assigned to the data points.
+    * point1, point2 : lists = [x, y]
+        Lists the x, y coordinates of the 2 points between which the profile
+        will be extracted.
+    * size : int
+        Number of points along the profile.
+    * algorithm : string
+        Interpolation algorithm. Either ``'cubic'``, ``'nearest'``,
+        ``'linear'`` (see scipy.interpolate.griddata).
+    Returns:
+    * [xp, yp, distances, vp] : 1d arrays
+        ``xp`` and ``yp`` are the x, y coordinates of the points along the
+        profile. ``distances`` are the distances of the profile points from
+        ``point1``. ``vp`` are the data points along the profile.
+    """
+    x1, y1 = point1
+    x2, y2 = point2
+    maxdist = np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+    distances = np.linspace(0, maxdist, size)
+    angle = np.arctan2(y2 - y1, x2 - x1)
+    xp = x1 + distances*np.cos(angle)
+    yp = y1 + distances*np.sin(angle)
+    vp = interp_at(x, y, v, xp, yp, algorithm=algorithm, extrapolate=True)
+    return xp, yp, distances, vp
