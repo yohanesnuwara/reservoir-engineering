3f3e654203bc458b511e14e4e9f2d5d324f3fcd1
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-02-20 19:04:56 +0700

    Create regression.py

---
+++ Unit 7 Introduction to Well-Test Analysis/functions/regression.py
@@ -0,0 +1,18 @@
+def regression(x, y): 
+    "Linear Regression"
+    import numpy as np
+    # number of observations/points 
+    n = np.size(x) 
+  
+    # mean of x and y vector 
+    m_x, m_y = np.mean(x), np.mean(y) 
+  
+    # calculating cross-deviation and deviation about x 
+    SS_xy = np.sum(y*x) - n*m_y*m_x 
+    SS_xx = np.sum(x*x) - n*m_x*m_x 
+  
+    # calculating regression coefficients 
+    b_1 = SS_xy / SS_xx 
+    b_0 = m_y - b_1*m_x 
+  
+    return(b_0, b_1) # b_0 is intercept, b_1 is slope
