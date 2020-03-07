b939b0b3446f1172b736481ca8b0fdb41dc7094c
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-03-07 21:40:38 +0700

    Create tdma_implicit.py

---
+++ Reservoir Simulation Ertekin/Unit 5 Finite-Difference Approximation to Linear-Flow Equations/tdma_implicit.py
@@ -0,0 +1,21 @@
+## Tri Diagonal Matrix Algorithm(a.k.a Thomas algorithm) solver
+def TDMAsolver(a, b, c, d):
+    '''
+    TDMA solver, a b c d can be NumPy array type or Python list type.
+    solving tridiagonal matrix for implicit (backward-difference) method
+    '''
+    import numpy as np
+    nf = len(d) # number of equations
+    ac, bc, cc, dc = map(np.array, (a, b, c, d)) # copy arrays
+    for it in range(1, nf):
+        mc = ac[it-1]/bc[it-1]
+        bc[it] = bc[it] - mc*cc[it-1] 
+        dc[it] = dc[it] - mc*dc[it-1]
+        	    
+    xc = bc
+    xc[-1] = dc[-1]/bc[-1]
+
+    for il in range(nf-2, -1, -1):
+        xc[il] = (dc[il]-cc[il]*xc[il+1])/bc[il]
+
+    return(xc)
