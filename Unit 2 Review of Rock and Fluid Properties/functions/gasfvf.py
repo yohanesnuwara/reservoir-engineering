daae7efc29f128b0dbca47bab4f3ff106c85380b
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-06-23 12:45:33 +0700

    Update gasfvf.py

--- Unit 2 Review of Rock and Fluid Properties/functions/gasfvf.py
+++ Unit 2 Review of Rock and Fluid Properties/functions/gasfvf.py
@@ -1,3 +1,20 @@
 def gasfvf(z, temp, pressure):
-  Bg = 0.0282793 * z * temp / pressure # Eq 2.2, temp in Rankine, p in psia
+  """
+  Gas FVF calculated in oilfield unit, result in RB/scf
+  inputs temp in Rankine (Fahrenheit + 460), pressure in psia or psig
+  """
+  Bg = 0.0282793 * z * temp / pressure 
+  return(Bg)
+
+def gasfvf(unit='unit1', temp=186, pressure=2000):
+  """
+  Gas FVF calculated in other units
+  unit: choice of units (unit1: RB/scf, unit2: res m3/std m3)
+  for unit1, inputs temp in Rankine (Fahrenheit + 460), pressure in psia or psig
+  for unit2, inputs temp in Kelvin, pressure in psia or psig
+  """
+  if unit == 'unit1':
+    Bg = 0.00503676 * z * temp / pressure 
+  if unit == 'unit2':
+    Bg = 0.350958 * z * temp / pressure
   return(Bg)
