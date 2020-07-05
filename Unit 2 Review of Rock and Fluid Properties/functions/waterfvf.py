899c93314d19e3b7757df2fd5af822d84abb8958
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2020-07-05 13:33:37 +0700

    Update waterfvf.py

--- Unit 2 Review of Rock and Fluid Properties/functions/waterfvf.py
+++ Unit 2 Review of Rock and Fluid Properties/functions/waterfvf.py
@@ -3,6 +3,6 @@ def waterfvf(temp, p):
   # temp in Fahrenheit
   # p pressure in psia
   Vwp = (-1.95301E-9 * p * temp) - (1.72834E-13 * (p**2) * temp) - (3.588922E-7 * p) - (2.25341E-10 * p**2)
-  Vwt = (-1E-2) + (1.33391E-2 * temp) + (5.50654E-7 * temp**2)
+  Vwt = (-1.001E-2) + (1.33391E-4 * temp) + (5.50654E-7 * temp**2)
   Bw = (1 + Vwt) * (1 + Vwp)
   return(Bw)
